bdd7523acc59465e18f5187a7272adc13c9f2dfe
fennellSAP <alexfennell95@gmail.com>
2019-07-16 15:23:42 -0400

    Cleaning code

--- logTime.py
+++ logTime.py
@@ -13,41 +13,38 @@ date = datetime.datetime.now()
 dateFormatted = date.strftime("%a") + "-" + date.strftime("%b") + "-" + date.strftime("%d") + "-" + date.strftime("%Y")
 dateXPath = '//*[@id="__jsview0--catsxtCalendar-' + dateFormatted + '"]'
 
-try:
-    element = WebDriverWait(driver, 6).until(
-        EC.presence_of_all_elements_located((By.XPATH, dateXPath)))
 
+try:
+    WebDriverWait(driver, 6).until(EC.presence_of_all_elements_located((By.XPATH, dateXPath)))
+    driver.find_element_by_xpath(dateXPath).click()
 except:
     print("Error finding date cell in calendar")
 
-try:
-    element = WebDriverWait(driver, 6).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="__jsview1--idDetailIconTabBar--header-arrowScrollRight"]')))
 
+try:
+    WebDriverWait(driver, 6).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="__jsview1--idDetailIconTabBar--header-arrowScrollRight"]')))
+    driver.find_element_by_xpath('//*[@id="__jsview1--idDetailIconTabBar--header-arrowScrollRight"]').click()
 except:
     print("Error finding arrow over button")
 
-driver.find_element_by_xpath('//*[@id="__jsview1--idDetailIconTabBar--header-arrowScrollRight"]').click()
-
 
 try:
-    element = WebDriverWait(driver, 6).until(
-        EC.presence_of_all_elements_located((By.ID, "__filter3-icon")))
+    WebDriverWait(driver, 6).until(EC.presence_of_all_elements_located((By.ID, "__filter3-icon")))
+    driver.find_element_by_id("__filter3-icon").click()
 
 except:
     print("Error finding time entry section button")
 
-driver.find_element_by_id("__filter3-icon").click()
 
 try:
-    element = WebDriverWait(driver, 6).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="__jsview1--detailsSaveButton"]')))
+    WebDriverWait(driver, 6).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="__jsview1--detailsSaveButton"]')))
+    driver.find_element_by_xpath('//*[@id="__jsview1--detailsSaveButton"]').click()
+    print("Time Has Been Entered")
 
 except:
     print("Error finding save button")
 
-driver.find_element_by_xpath('//*[@id="__jsview1--detailsSaveButton"]').click()
-print("Time Has Been Entered")
+
 
 
 
