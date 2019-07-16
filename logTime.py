db30663da604c7ff0369b088130ea90aefacec38
fennellSAP <alexfennell95@gmail.com>
2019-07-16 16:54:53 -0400

    Added chrome headless

--- logTime.py
+++ logTime.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
 import datetime
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=options)
 
 def clickSomething(selector):
 
